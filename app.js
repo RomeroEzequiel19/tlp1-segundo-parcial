ddcfcd6f536b457e2dd8028ff13398e8eb28edef
Carlos Ezequiel Romero <ezequielromero3039@gmail.com>
2023-06-21 14:51:47 -0300

    Se realizó importaciones de morgan y dotenv en el archivo app.js, se implementaron los middlewares, se modificó el puerto de escucha, y se colocó el error 404

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 // Imports
-const cors = require('cors');
 const express = require('express');
-
+const cors = require('cors');
+const morgan = require('morgan');
 const path = require('path');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 6000;
 
 const app = express();
 
+
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,5 +25,21 @@ app.use('/api', require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
+app.use((req, res, next) => {
+    res.write(`<div>
+        <h1>La Ruta se encuentra</h1>
+        <hr>
+        <p>La pagina que intentas buscar no existe</p>
+        <p>Se redireccionará a la página de inicio</p>
+        <script>
+        (
+          () => setTimeout(() => {
+            window.location.href='http://localhost:${port}/aeropuerto';
+           }, 3000)           
+        )();
+        </script>
+    </h1>`)
+});
+
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log(`Servidor correindo en http://localhost:${port}`));
