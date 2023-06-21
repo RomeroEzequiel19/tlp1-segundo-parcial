659640bd0df3e0893af8ce1c842e50a13dfda2d7
Carlos Ezequiel Romero <ezequielromero3039@gmail.com>
2023-06-21 15:42:04 -0300

    Se realizaron las rutas

--- routes/reserva.routes.js
+++ routes/reserva.routes.js
@@ -10,29 +10,37 @@ const router = require('express').Router();
 
 
 // Obtener todas las reservas
-
+router.get('/',(req,res)=>{
+    res.render('index')
+});
 // Formulario para crear una reserva
-
+router.get("/reservaa/crear", (req, res) => {
+    res.render("reservas/crear_reserva");
+  });
 // Formulario para actualizar una reserva
-
+router.get("/reserva/editar/:id", (req, res) => {
+    res.render("reserva/editar_persona", { id: req.params.id });
+  });
+  
 // ==========================================
 //         Rutas para CRUD de reservas
 // ==========================================
 
-router.get('/',(req,res)=>{
-    res.render('index')
-});
-// Obtener todas las reservas
 
+// Obtener todas las reservas
+router.get("/api/reserva", obtenerReserva);
+
+//Obtener una reserva
+router.get("/api/reserva/:id", obtenerReserva);
  
 // Crear una reserva
-router.post('/api/',);
+router.post("/api/reserva", crearReserva);
  
 // Actualizar una reserva
-router.put('/api/:id',);
+router.put("/api/reservaa/:id", actualizarReserva);
  
 // Eliminar una reserva de forma lógica
-router.delete('/api/:id',);
+router.delete("/api/reserva/:id", eliminarReserva);
 
  
  module.exports = router;
