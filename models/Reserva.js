54b6fa2ec6a0c50191f1575a32eaa64867b1ee01
Carlos Ezequiel Romero <ezequielromero3039@gmail.com>
2023-06-21 15:05:05 -0300

    Se instaló dotenv y se creó el modelo de datos para la reserva

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,56 @@
 // TODO: Crear modelo de datos de Reserva
+
+const { sequelize, DataTypes } = require('../db');
+
+const Reserva = sequelize.define('Reserva', {
+    // Model attributes are defined here
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    apellido: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    DNI: {
+        type: DataTypes.INTEGER,
+        defaultValue: false
+    },
+    telefono: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    estado: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+    cod_reserva: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    fecha_reserva: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    // Other model options go here
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: 'reserva'
+});
+
+// Crear tabla si no existe
+Reserva.sync();
+
+module.exports = Reserva;
